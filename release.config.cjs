4c9cb0d337d034b11d398422840808e54c485593
kitt.digital <169592437+kitt-digital@users.noreply.github.com>
2026-05-16 18:25:28 +0200

    feat(breaking): + set up project

---
+++ release.config.cjs
@@ -0,0 +1,47 @@
+/**
+ * Semantic Release configuration file.
+ *
+ * Used to automate the package release workflow
+ * by your commit messages.
+ *
+ * @type {import('semantic-release').GlobalConfig}
+ *
+ * Configuration of the git commit-msg hook:
+ * @ALLOWED_MESSAGE_TYPES=[minor, feat, patch, fix, style, test];
+ * This is necessary to validate the commit messages locally
+ */
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'minor', release: 'minor' },
+          { type: 'feat', release: 'minor' },
+          { type: 'patch', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'test', release: false },
+          { type: 'perf', release: false },
+          { scope: 'no-release', release: false },
+          { scope: 'breaking', release: 'major' }
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+      }
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'src/@types/**/*.d.ts'],
+        message: '[RELEASE]: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ]
+};
