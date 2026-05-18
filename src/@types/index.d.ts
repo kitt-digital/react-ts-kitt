ae486e0f5b7bcc2d69ec265c9d55aa7267bad866
kitt.digital <169592437+kitt-digital@users.noreply.github.com>
2026-05-18 07:53:51 +0200

    fix(typescript): ~ dts

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,4 @@
 declare const test: string;
 declare const test2: string;
-export { test, test2 };
+declare const test3: number;
+export { test, test2, test3 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 const test: string = "Hello, World!";
-const test2: string = "Hello, TypeScript.";
+const test2: string = "Hello, TypeScript!";
+const test3: number = 42;
 
-export {test, test2};
+export {test, test2, test3};
